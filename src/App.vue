3ce20949e5796b62a137182aac6960f89c507e54
fitriamalia <fitriamalia200222@gmail.com>
2023-03-28 16:43:56 +0700

    Commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <h1>WELCOME TO UIN SUSKA RIAU</h1>
+  <h1>Welcome To Android</h1>
     <img alt="Vue logo" class="logo" src="./assets/logoandroid.png" width="250" height="250" />
 </template>
 
