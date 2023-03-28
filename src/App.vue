359652853526a6b61a8c31c9272af2e456b91e1e
fitriamalia <fitriamalia200222@gmail.com>
2023-03-28 14:47:50 +0700

    Commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <h1>SELAMAT DATANG DI UIN SUSKA RIAU</h1>
+  <h1>WELCOME TO UIN SUSKA RIAU</h1>
     <img alt="Vue logo" class="logo" src="./assets/uin-suska.png" width="250" height="250" />
 </template>
 
